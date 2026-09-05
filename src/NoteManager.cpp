fc9809c44a70acd550f5f26c68804837e59bb576
Carter Irish <2023c.irish@gmail.com>
2026-09-04 21:26:58 -0400

    Feat: Began implementation of internal data structure to hold the notes dir as a tree
    - NoteManager::buildTree navigates the notes dir and returns a rootNode to navigate the dir from

--- src/NoteManager.cpp
+++ src/NoteManager.cpp
@@ -1,6 +1,28 @@
 #include "NoteManager.h"
+#include <filesystem>
 
-void NoteManager::scan(const std::string& rootDir) {}
+
+
+void NoteManager::scan(const std::string& rootDir) {
+}
+
+DirNode NoteManager::buildTree(const std::string& dirPath) {
+    std::vector<Note> notes;
+    std::vector<std::unique_ptr<DirNode>> subDirs;
+    std::filesystem::directory_iterator dirIter(dirPath);
+
+    for (const auto& entry : dirIter) {
+        if (entry.is_directory()) {
+			// Recursively build the tree for the subdirectory
+			subDirs.push_back(std::make_unique<DirNode>(buildTree(entry.path().string())));
+        }
+        else {
+            // Base case: create a Note object for the file, push to vector
+            notes.push_back(Note(entry.path().filename().string(), entry.path().string()));
+        }
+    }
+    return DirNode(dirPath, std::move(notes), std::move(subDirs));
+}
 
 const std::vector<Note>& NoteManager::getNotes() const {
     return notes;
--- src/NoteManager.h
+++ src/NoteManager.h
@@ -1,10 +1,22 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <memory>
 
 struct Note {
     std::string filename;
     std::string path;
+
+	Note(std::string filename, std::string path) : filename(filename), path(path) {}
+};
+
+struct DirNode {
+    std::string name;
+    std::vector<Note> notes;
+    std::vector<std::unique_ptr<DirNode>> subDirs;
+
+    DirNode(std::string name, std::vector<Note> notes, std::vector<std::unique_ptr<DirNode>> subDirs)
+        : name(std::move(name)), notes(std::move(notes)), subDirs(std::move(subDirs)) {}
 };
 
 class NoteManager {
@@ -13,6 +25,8 @@ public:
     const std::vector<Note>& getNotes() const;
     void viewNote(const Note& note);
     void editNote(const Note& note);
+    std::vector<Note> flattenAll(const std::string& rootDir);
+	DirNode buildTree(const std::string& dirPath);
 
 private:
     std::vector<Note> notes;
